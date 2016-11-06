c6251844ed07ac0697a508b9c46c143072f54d40
原田　礼朗 <rharada@MBA-11AJ-090.local>
2016-11-06 23:53:03 +0900

    first commit

---
+++ yamadaApp/MenuCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCollectionViewCell.swift
+//  yamadaApp
+//
+//  Created by 原田　礼朗 on 2016/11/06.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ yamadaApp/TextCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TextCollectionViewCell.swift
+//  yamadaApp
+//
+//  Created by 原田　礼朗 on 2016/11/06.
+//  Copyright © 2016年 reo harada. All rights reserved.
+//
+
+import UIKit
+
+class TextCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
